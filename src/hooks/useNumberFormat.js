d8f723572c6bc566bf9b1c9cc706d3ecabac605e
unknown <emir.karic1@outlook.com>
2020-09-29 21:33:48 +0200

    Removed api call and modified useNumberFormat

--- src/hooks/useNumberFormat.js
+++ src/hooks/useNumberFormat.js
@@ -1,47 +1,24 @@
 import { useState, useEffect } from "react";
-import { getCountryInfo } from "utils/geoNamesApi";
-
-function setDefaultLocale() {
-  return (
-    navigator.userLanguage ||
-    navigator.language ||
-    navigator.browserLanguage ||
-    navigator.systemLanguage ||
-    "bs"
-  );
-}
 
 export const useNumberFormat = () => {
-  const [position, setPosition] = useState({});
-  const [local, setLocal] = useState(setDefaultLocale());
+  const [local, setLocal] = useState('');
 
   useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (_position) => {
-          setPosition({
-            lat: _position.coords.latitude,
-            lng: _position.coords.longitude,
-          });
-        },
-        () => {
-          console.log("User don't want to share position ");
-        }
-      );
-    } else {
-      console.log("Using an old browser that doesn't support geolocation");
-    }
+    setLocal(
+      navigator.userLanguage ||
+      navigator.language ||
+      navigator.browserLanguage ||
+      navigator.systemLanguage ||
+      "bs-BA"
+    );
   }, []);
 
-  useEffect(()=>{
-    getCountryInfo(position).then(({languages}) => {
-      languages && setLocal(languages.split(",")[0]);
-    })
-  }, [position]);
-
-  console.log(local);
-  
-  return { formatNumberOutput(number) {
+  const formatNumberOutput = (number) => {
     return new Intl.NumberFormat(local).format(number);
-  }};
+  }
+
+  
+  return { 
+    formatNumberOutput,
+  };
 };
